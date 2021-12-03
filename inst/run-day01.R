1ad33a1a40efed71539a006a29e0edd37e81fe52
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-12-03 12:34:56 +1100

    Solved Day 1 puzzles!
    * used {slider} for the first time
    * test ans fncs against example data

---
+++ inst/run-day01.R
@@ -0,0 +1,9 @@
+library(adventofcode21)
+x <- as.numeric(readLines("./inst/input01.txt"))
+
+p1 <- f01a(as.numeric(x))
+p2 <- f01b(x)
+
+stopifnot(p1 == aoc_solutions$day01a)
+stopifnot(p2 == aoc_solutions$day01b)
+
---
+++ tests/testthat/test-day01.R
@@ -0,0 +1,3 @@
+test_that("f01b works", {
+  expect_equal(f01b(example_data_01()), 5)
+})
